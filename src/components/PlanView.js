747da3b3a3842918f95a98668b397b163482bc78
Nathan Rowe <nathanrowe@bell.net>
2022-01-21 14:48:18 -0500

    Implement modal to add exercise to plan

--- src/components/PlanView.js
+++ src/components/PlanView.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PaperClipIcon } from '@heroicons/react/outline'
 import { PlusCircleIcon, PencilIcon, TrashIcon } from '@heroicons/react/solid'
 
+import AddExerciseModal from "./AddExerciseModal";
+
 export default function PlanView({ plan, setShowDirectory }) {
+
+  const [addExerciseModalOpen, setAddExerciseModalOpen] = useState(false)
 
   return (
     <div className="bg-white shadow overflow-hidden sm:rounded-lg min-w-full xl:pr-96">
@@ -43,50 +47,14 @@ export default function PlanView({ plan, setShowDirectory }) {
               {plan.exercises.length}
             </dd>
           </div>
-          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">About</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur
-              qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud
-              pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
-            </dd>
-          </div>
-          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-500">Attachments</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              <ul className="border border-gray-200 rounded-md divide-y divide-gray-200">
-                <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                  <div className="w-0 flex-1 flex items-center">
-                    <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                    <span className="ml-2 flex-1 w-0 truncate">resume_back_end_developer.pdf</span>
-                  </div>
-                  <div className="ml-4 flex-shrink-0">
-                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                      Download
-                    </a>
-                  </div>
-                </li>
-                <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                  <div className="w-0 flex-1 flex items-center">
-                    <PaperClipIcon className="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                    <span className="ml-2 flex-1 w-0 truncate">coverletter_back_end_developer.pdf</span>
-                  </div>
-                  <div className="ml-4 flex-shrink-0">
-                    <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                      Download
-                    </a>
-                  </div>
-                </li>
-              </ul>
-            </dd>
-          </div>
         </dl>
       </div>
       <div className="flex flex-row justify-between items-center px-2 py-2">
-        <h3 className="text-lg font-medium text-gray-900">Exercises</h3>
+        <h3 className="ml-2 text-lg font-medium text-gray-900">Exercises</h3>
         <button
           type="button"
           className="inline-flex justify-center text-sm font-medium rounded-md bg-white"
+          onClick={() => setAddExerciseModalOpen(true)}
         >
           <PlusCircleIcon className="-ml-1 mr-2 h-5 w-5 text-indigo-600 hover:text-indigo-500" aria-hidden="true" />
         </button>
@@ -158,6 +126,7 @@ export default function PlanView({ plan, setShowDirectory }) {
           </div>
         </div>
       </div>
+      <AddExerciseModal open={addExerciseModalOpen} setOpen={setAddExerciseModalOpen}/>
     </div>
   )
 }
